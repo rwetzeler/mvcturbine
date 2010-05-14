fbcdb4f641661b775deb9b06276f8834d4044bfc
Rob Wetzeler <rwetzeler@wetzelerweb.com>
2010-05-14 14:11:57 -0500

    Added basic EntityFrameworkProfiler Blade

---
+++ src/Blades/EntityFrameworkProfiler/src/MvcTurbine.EntityFrameworkProfiler/EntityFrameworkProfilerBlade.cs
@@ -0,0 +1,19 @@
+﻿namespace MvcTurbine.EntityFrameworkProfiler
+{
+	using Blades;
+
+	public class EntityFrameworkProfilerBlade : Blade
+	{
+		#region Overrides of Blade
+
+		/// <summary>
+		/// Executes the current blade.
+		/// </summary>
+		public override void Spin(IRotorContext context)
+		{
+			HibernatingRhinos.Profiler.Appender.EntityFramework.EntityFrameworkProfiler.Initialize();
+		}
+
+		#endregion
+	}
+}
